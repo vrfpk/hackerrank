e5e8b6bf38502257abcf411d320ad4d478e3b929
Michal Wichulski <m.wichulski@gmail.com>
2016-06-27 19:01:13 +0200

    is_subset added

--- other/find_max_partition/main.c
+++ other/find_max_partition/main.c
@@ -6,7 +6,7 @@
 #define FALSE 0
 #define TRUE 1
 
-int ** alloc2d(int n, int m)
+int** alloc2d(int n, int m)
 {
 	int i;
 	int **p = (int**) malloc(n * sizeof(int*));
@@ -15,10 +15,37 @@ int ** alloc2d(int n, int m)
 	return p;
 }
 
-void delete2d(int **p)
+void free2d(int **p)
 {
 	free(p[0]);
 	free(p);
+}
+
+int compare(const void* p1, const void *p2)
+{
+	if (*(int*) p1 <  *(int*) p2) return -1;
+	if (*(int*) p1 == *(int*) p2) return 0;
+	return 1;
+}
+
+/* Return 1 if Y is a subset of X */
+int is_subset(int X[], int Y[], int n, int m)
+{
+	int i = 0, j = 0;
+	if (n < m) return 0;
+	qsort(X, n, sizeof(int), compare);
+	qsort(Y, m, sizeof(int), compare);
+	while (i < n && j < m) {
+		if (X[i] < Y[j]) {
+			i++;
+		} else if (X[i] == Y[j]) {
+			++i; ++j;
+		} else if (X[i] > Y[j]) {
+			return 0;
+		}
+	}
+	if (j < m) return 0;
+	else return 1;
 }
 
 int find_max_partition(int arr[], int n)
@@ -26,13 +53,17 @@ int find_max_partition(int arr[], int n)
 	int sum = 0;
 	int i, j, s, ret = 0;
 	int **dp;
-	int tmp1, tmp2, element, check;
+	int p, tmp2, element, check;
+	int *a1, *a2;
+	int cnt1, cnt2;
+
 	for (i = 0; i < n; ++i)
 		sum += arr[i];
 
 	if (sum % 2 != 0)
 		sum -= 1;
 
+	/* initialize dp array */
 	dp = alloc2d(sum + 1, n + 1);
 	for (i = 0; i < n + 1; ++i)
 		dp[0][i] = TRUE;
@@ -40,6 +71,12 @@ int find_max_partition(int arr[], int n)
 	for (i = 1; i < sum + 1; ++i)
 		dp[i][0] = FALSE;
 
+	/* build dp array
+	 * dp[s][i] is true if set Ai: { arr[0], ..., arr[i-1] }
+	 * have subset with sum = s. There are two cases:
+	 * a) arr[i-1] is part of that subset, so exclude it
+	 *    from from Ai and take into account sum = s - arr[i-1]
+	 * b) arr[i-1] is not part of that subset, so exclude it but still sum = s; */
 	for (s = 1; s <= sum; s++) {
 		for (i = 1; i <= n; i++) {
 			dp[s][i] = dp[s][i - 1];
@@ -48,67 +85,63 @@ int find_max_partition(int arr[], int n)
 		}
 	}
 
-	//for (s = sum / 2; s >= 1; s = s - 1) {
-	//	if (dp[2 * s][n] == TRUE && dp[s][n] == TRUE) {
-	//		ret = s;
-	//		break;
-	//	}
-	//}
-
-	//printf("\n");
-
 	//for (s = sum; s >= 1; --s) {
 	//	if (dp[s][n] == FALSE) continue;
 	//	printf("%02d: ", s);
-	//	tmp1 = s;
-	//	while (tmp1 != 0) {
-	//		for (i = n; i >= 1; --i) if (dp[tmp1][i] == TRUE && dp[tmp1][i - 1] == FALSE) {
-	//			printf("[%02d %d %02d] ", tmp1, i - 1, arr[i - 1]);
-	//			tmp1 -= arr[i - 1];
+	//	p = s;
+	//	while (p != 0) for (i = n; i >= 1; --i) if (dp[p][i] == TRUE && dp[p][i - 1] == FALSE) {
+	//			printf("[%d %02d]", i - 1, arr[i - 1]);
+	//			p -= arr[i - 1];
 	//			break;
 	//		}
-	//	}
 	//	printf("\n");
 	//}
 	//printf("\n");
+
+	a1 = (int*) malloc(n * sizeof(int));
+	a2 = (int*) malloc(n * sizeof(int));
+
+	/* If set Arr: { arr[0], ..., arr[n-1] } has two
+	 * disjoint subsets with the same sum, Arr has to have
+	 * subset A1 with sum = x and A2 with sum = 2*x, and
+	 * moreover A1 has to be subset of A2 */
 	for (s = sum / 2; s >= 1; --s) {
 		if (dp[s][n] == FALSE) continue;
 		if (dp[2 * s][n] == FALSE) continue;
-		tmp1 = s;
-		while (tmp1 != 0) {
-			for (i = n; i >= 1; --i) if (dp[tmp1][i] == TRUE && dp[tmp1][i - 1] == FALSE) {
-				element = arr[i - 1];
-				tmp2 = 2 * s;
-				check = 0;
-				while (tmp2 != 0) {
-					for (j = n; j >= 1; --j) if (dp[tmp2][j] == TRUE && dp[tmp2][j - 1] == FALSE) {
-						if (arr[j - 1] == arr[i - 1]) {
-							check = 1; tmp2 = 0; break;
-						}
-						tmp2 -= arr[j - 1];
-					}
-				}
-				if (check == 0) { tmp1 = 0; break; }
-				tmp1 -= arr[i - 1];
+		cnt1 = cnt2 = 0;
+		p = s;
+		while (p != 0)
+			for (i = n; i >= 1; --i) if (dp[p][i] == TRUE && dp[p][i - 1] == FALSE) {
+				a1[cnt1++] = arr[i - 1];
+				p -= arr[i - 1];
 				break;
 			}
-		}
-		if (check == 1) {
-			ret = s; break;
+		p = 2 * s;
+		while (p != 0)
+			for (i = n; i >= 1; --i) if (dp[p][i] == TRUE && dp[p][i - 1] == FALSE) {
+				a2[cnt2++] = arr[i - 1];
+				p -= arr[i - 1];
+				break;
+			}
+		if (1 == is_subset(a2, a1, cnt2, cnt1)) {
+			ret = s;
+			break;
 		}
 	}
 
-	delete2d(dp);
+	free(a2);
+	free(a1);
+	free2d(dp);
 
 	return ret;
 }
 
 void test_find_max_partition()
 {
-	 int n = 4;
+	 int n = 5;
 	 int p = 0;
 	 //int i;
-	 int arr[] = { 1, 3, 5, 10 };
+	 int arr[] = { 2, 4, 6, 8, 10 };
 
 	 if (0 == (p = find_max_partition(arr, n)))
 		printf("arr cannot be equally devided\n");
